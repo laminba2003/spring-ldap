8e4a2d192b649b65ea0a6a9bb6b783bcb39d1014
mamadou lamine ba <laminba2003@gmail.com>
2022-12-09 22:18:04 +0000

    improvements

--- src/main/java/com/spring/training/exception/APIExceptionHandler.java
+++ src/main/java/com/spring/training/exception/APIExceptionHandler.java
@@ -2,6 +2,7 @@ package com.spring.training.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,6 +15,13 @@ public class APIExceptionHandler {
     @ExceptionHandler(value = {NamingException.class})
     public ResponseEntity<APIException> handleEntityNotFoundException(NamingException e) {
         APIException exception = new APIException(e.getMessage(),
+                HttpStatus.BAD_REQUEST, LocalDateTime.now());
+        return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<APIException> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        APIException exception = new APIException("the input provided is invalid",
                 HttpStatus.BAD_REQUEST, LocalDateTime.now());
         return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
     }
